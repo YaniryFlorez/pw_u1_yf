daa0f472ca8d1e2ab5e574a95494221abfa78a0b
YaniryFlorez <ymflorezrivera@gmail.com>
2025-05-01 19:42:07 -0500

    Taller 10 Calculadora Maquetacion

---
+++ js/calculadorafinal.js
@@ -0,0 +1,19 @@
+let num1 =0;
+let num2 =0;
+let esSegundoDigito ="0";
+
+
+function mostrarEnDisplay(valor) {
+    let elementoDisplay = document.getElementById("id_display");
+    elementoDisplay.innerText =elementoDisplay.innerText + valor;
+    if(valor !==)
+}
+
+function operacionSumar() {
+    if(esSegundoDigito === "0"){
+        num1 = parseFloat(document.getElementById("id_display").innerText);
+        esSegundoDigito = "1";     
+
+    }
+    
+}
--- js/formulario.js
+++ js/formulario.js
@@ -1,13 +1,57 @@
 function guardar(){
-validar();
+  validar();
+  //validar todos los campos Tarea.
+} 
 
 function validar(){
- let nombre = document.getElementById("id_nombre").value;
- if (nombre == "") {
-    alert("Error no ha ingresado el nombre");
-   
-  }else{
-    console.log("el nombre no puede estar vacio");
+  let nombre = document.getElementById('id_nombre').value;
+  if (nombre =="") {
+    console.log("Error no ha ingresado el nombre");
+    
+    
+  } else {
+    console.log("Nombre ingresado correctamente");
+  
   }
+  //validar el campo apellido
+  let apellido = document.getElementById('id_apellido').value;  
+  if (apellido =="") {
+    console.log("Error no ha ingresado el apellido");
+    
+    
+  } else {
+    console.log("Apellido ingresado correctamente");
+  
+  }
+  //validar el campo Fecha de nacimiento
+  let fecha_nacimiento = document.getElementById('id_fecha_nacimiento').value;
+  if (fecha_nacimiento =="") {
+    console.log("Error no ha ingresado la fecha de nacimiento");
+    
+  } else {
+    console.log("Fecha de nacimiento ingresada correctamente");
+  
+  }
+
+ 
+  //validar el campo correo
+  let correo = document.getElementById('id_correo').value;  
+  if (correo =="") {
+    console.log("Error no ha ingresado el correo");
+  }
+  else {
+    console.log("Correo ingresado correctamente");
+  
+  }
+  //validar el campo password
+  let password = document.getElementById('id_password').value;
+  if (password =="") {
+    console.log("Error no ha ingresado la contraseña");
+    
+  } else {
+    console.log("Contraseña ingresada correctamente");
+  
+  }
+
 }
    
